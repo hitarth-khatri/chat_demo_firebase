ccecfff76ffdc61e3c7f65cd5fa7f3f204b9ab4c
hitarth.chhunchha <hitarthkhatri123@gmail.com>
2023-03-06 19:01:45 +0530

    text field as bottomSheet

--- lib/app/chat/view/chat_screen.dart
+++ lib/app/chat/view/chat_screen.dart
@@ -14,7 +14,6 @@ class ChatScreen extends GetView<ChatController> {
     double width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomInset: true,
         appBar: AppBar(
           title: Row(
             children: [
@@ -25,62 +24,58 @@ class ChatScreen extends GetView<ChatController> {
             ],
           ),
         ),
-        body: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            FirebaseAnimatedList(
-              primary: false,
-              padding: const EdgeInsets.all(5),
-              defaultChild: const Center(child: CircularProgressIndicator()),
-              query: controller.dbQuery,
-              itemBuilder: (context, snapshot, animation, index) {
-                final json = snapshot.value as Map;
-                final messageModel = MessageModel.fromJson(json);
-                return controller.senderId == messageModel.senderId
-                    ?
-                    //sender column right
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          chatTile(
-                            alignment: Alignment.centerRight,
-                            width: width,
-                            message: messageModel.message,
-                          ),
-                        ],
-                      )
-                    :
-                    //receiver column left
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          chatTile(
-                            alignment: Alignment.centerLeft,
-                            width: width,
-                            message: messageModel.message,
-                          ),
-                        ],
-                      );
-              },
-            ),
-            //send message text field
-            Container(
-              color: AppColors.white,
-              child: TextFormField(
-                controller: controller.msgController,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  labelText: "Enter message",
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      controller.sendMessage();
-                    },
-                    icon: const Icon(Icons.send),
-                  ),
-                ),
+        body: FirebaseAnimatedList(
+          primary: false,
+          padding: const EdgeInsets.all(5),
+          defaultChild: const Center(child: CircularProgressIndicator()),
+          query: controller.dbQuery,
+          itemBuilder: (context, snapshot, animation, index) {
+            final json = snapshot.value as Map;
+            final messageModel = MessageModel.fromJson(json);
+            return controller.senderId == messageModel.senderId
+                ?
+                //sender column right
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      chatTile(
+                        alignment: Alignment.centerRight,
+                        width: width,
+                        message: messageModel.message,
+                      ),
+                    ],
+                  )
+                :
+                //receiver column left
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      chatTile(
+                        alignment: Alignment.centerLeft,
+                        width: width,
+                        message: messageModel.message,
+                      ),
+                    ],
+                  );
+          },
+        ),
+        //send message text field
+        bottomSheet: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          color: AppColors.white,
+          child: TextFormField(
+            controller: controller.msgController,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              hintText: "Enter message",
+              suffixIcon: IconButton(
+                onPressed: () {
+                  controller.sendMessage();
+                },
+                icon: const Icon(Icons.send),
               ),
-            ).paddingSymmetric(horizontal: 8),
-          ],
+            ),
+          ),
         ),
       ),
     );
